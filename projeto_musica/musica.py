2d536892f1381e9e1d2fc8446e4d6f165de8c7d4
Bruno César <brunocesarglm@gmail.com>
2024-11-11 09:20:52 -0300

    projeto musica 2

--- projeto_musica/musica.py
+++ projeto_musica/musica.py
@@ -1,8 +1,10 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask, render_template, request, redirect,send_from_directory
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
+import os
 
+UPLOAD_PASTA = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'oracle+cx_oracle://{usuario}:{senha}@\
@@ -15,56 +17,106 @@ sid = 'xe'
 )
 db = SQLAlchemy(app)
 
+
 @app.route('/testeBD')
 def testar_conexao():
     try:
         resultado = db.session.execute(text('SELECT * FROM MUSICAS')).fetchall()
-        print(resultado)
+        print (resultado)
         if(resultado):
-            return "Conexao bem-sucedida"
+            return "Conexão bem-sucedida"
         else:
-            return "conexao Falhou"
+            return "Conexão Falhou!"
     except Exception as e:
         return f"Erro ao tentar conectar: {str(e)}"
-     
-lista_musicas = [
-        {"nome":"Musica1", "Artista": "Artista1", "Genero": "Genero1"},
-        {"nome":"Show das poderosas", "Artista": "Anitta", "Genero": "Funk"},
-        {"nome":"Empreguetes", "Artista": "3 marias", "Genero": "POP"},
-]
-@app.route('/inicio')
-def hello():
-    return "<h1>Hello World</h1>"
 
-@app.route('/musicas')
+@app.route('/')
 def listarMusicas():
     resultado = db.session.execute(text('SELECT * FROM MUSICAS')).fetchall()
     lista_musicas = []
     for registro in resultado:
         musica = {
             'nome':registro[0],
-            'Artista':registro[1],
+            'Artista': registro[1],
             'Genero':registro[2]
         }
         lista_musicas.append(musica)
     
-    return render_template('listar_musica.html', 
-                           titulo="Musicas preferidas dos Alunos",
+
+    return render_template('listar_musicas.html',
+                           titulo = "Músicas Preferidas dos Alunos",
                            musicas = lista_musicas)
+
 @app.route("/cadastrar")
 def cadastra_musica():
     return render_template("cadastrar_musicas.html")
 
-@app.route("/adicionar", methods = ['POST'])
+@app.route("/adicionar", methods=['POST',])
 def adicionar_musica():
     nome = request.form['txtNome']
     artista = request.form['txtArtista']
     genero = request.form['txtGenero']
-    novaMusica = {"nome":nome,"Artista": artista, "Genero":genero}
-    lista_musicas.append(novaMusica)
-    #return render_template('listar_musica.html', 
-    #                       titulo="Musicas preferidas dos Alunos",
+    #novaMusica = {"nome": nome, "Artista": artista, "Genero": genero}
+    #return render_template('listar_musicas.html',
+    #                       titulo = "Músicas Preferidas dos Alunos",
     #                       musicas = lista_musicas)
-    return redirect('/musicas')
+    comando = f"INSERT INTO MUSICAS VALUES('{nome}','{artista}','{genero}')"
+    db.session.execute(text(comando))
+    db.session.commit()
+    arquivo = request.files['arquivo']
+    arquivo.save(f'{UPLOAD_PASTA}/album{nome}.jpg')
+    return redirect('/')
 
+@app.route("/editar/<string:nome>")
+def editar(nome):
+    comando = f"SELECT * FROM MUSICAS WHERE nome like '{nome}'"
+    musicaBuscada = db.session.execute(text(comando)).fetchone()
+    return render_template('editar_musicas.html',
+                           musica = musicaBuscada)
+
+@app.route("/atualizar", methods=['POST',])
+def atualizar_musica():
+    nome = request.form['txtNome']
+    comando = f"SELECT * FROM MUSICAS WHERE nome like '{nome}'"
+    musicaBuscada = db.session.execute(text(comando)).fetchone()
+
+    artista = request.form['txtArtista']
+    genero = request.form['txtGenero']
+
+    comando = f"UPDATE MUSICAS SET artista = '{artista}',genero='{genero}' WHERE nome like '{nome}'"
+    
+    db.session.execute(text(comando))
+    db.session.commit()
+    arquivo = request.files['arquivo']
+    nome_arquivo = arquivo.filename
+    extensao_arquivo = nome_arquivo.split('.')[1]
+    arquivo.save(f'{UPLOAD_PASTA}/album{nome}.es')
+    return redirect('/')
+
+@app.route("/excluir/<string:nome>")
+def excluir(nome):
+    comando = f"DELETE FROM MUSICAS WHERE nome like '{nome}'"
+    db.session.execute(text(comando))
+    db.session.commit()
+    return redirect("/")
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if request.form['txtSenha'] == 'admin':
+        return redirect('/')
+    else:
+        return redirect('/login')
+def recupera_imagem(nome):
+    for nome_imagem in os.listdir(UPLOAD_PASTA):
+        nomeSemExtensao = str(nome_imagem).split('.')[0]
+        if nomeSemExtensao == f'album{nome}':
+            return nome_imagem
+        return 'defalut.png'
+@app.route('/uploas/<nome_imagem>')
+def imagem(nome_imagem):
+    return send_from_directory('uploads',nome_imagem)
 app.run(debug=True)
